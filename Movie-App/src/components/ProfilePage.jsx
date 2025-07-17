292b7671998adae9fa4e441cc5f55f7a975a3c2f
Ravi Kalia <kravi014@gmail.com>
2025-07-17 06:14:31 -0400

    profile page changes done

--- Movie-App/src/components/ProfilePage.jsx
+++ Movie-App/src/components/ProfilePage.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const ProfilePage = () => {
   return (
     <div className="profile-page">
+      <div className="profile-img">
+        <img src="/profileimage.jpg" alt="Profile" className="profile-picture" />
+      </div>
       <div className="data">
       <h2>Your Profile</h2>
       <p>Name: Ravi Kumar</p>
@@ -11,7 +14,7 @@ const ProfilePage = () => {
       <p>Course: Programming-2</p>
       <p>College: Humber College</p>
       <p>Program: Web Design and Development</p>
-      <p>GitHub: <a href="" target="_blank" rel="noopener noreferrer"></a></p>
+      <p>GitHub: <a href="https://github.com/ravikalia-01/Programming2-Project2.git" target="_blank" rel="noopener noreferrer">https://github.com/ravikalia-01/Programming2-Project2.git</a></p>
       {/* Add more user details */}
       </div>
     </div>
